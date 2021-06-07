5ef66cb4e6efee752c5bb2d1335c977b7acb1c1a
Alexandra Fossum <fossum.alexandra@gmail.com>
2021-06-07 07:58:29 -0500

    pulled from master, brought PrimaryCTA more in line with style guide

--- src/components/PrimaryCTA/index.js
+++ src/components/PrimaryCTA/index.js
@@ -5,39 +5,41 @@ import Link from "../Link/Link"
 
 const PrimaryCTA = ({ items, link, ctaText }) => {
   return (
-    <Box bgColor={`black`} maxWidth="100%" position="relative">
+    <Box bgColor={`#16171d`} maxWidth="100%" position="relative" py={19}>
       <Container
         display={["grid", "grid", "grid", "flex"]}
         placeItems="center"
         alignItems="center"
-        justifyContent="space-between"
-        py={10}
+        justifyContent="space-evenly"
+        py={[10, 15, 15, 15]}
         maxW={`1300px`}
       >
         <Box
-          display={["grid", "grid", "flex"]}
-          gridTemplateColumns={[
-            `repeat(3, 1fr)`,
-            `repeat(3, 1fr)`,
-            `repeat(3, 1fr)`,
-            `repeat(6, 1fr)`,
-          ]}
-          gridGap={2}
+          // display={["grid", "grid", "flex"]}
+          // gridTemplateColumns={[
+          //   `repeat(3, 1fr)`,
+          //   `repeat(3, 1fr)`,
+          //   `repeat(3, 1fr)`,
+          //   `repeat(6, 1fr)`,
+          // ]}
+          // gridGap={2}
           textAlign="center"
           alignItems="center"
           minHeight="max-content"
           width="100%"
+          px={15}
+          my={6}
         >
           {items.map(item => (
-            <Link to={item.link} color="white">
+            <Link to={item.link} color="white" style={{ textTransform: `uppercase`, fontSize: `20px` }}>
               {item.title}{" "}
-              <Text as="span" color="secondary">
-                |
+              <Text as="span" color="secondary" style={{ fontSize: `20px` }}>
+                |{" "}
               </Text>
             </Link>
           ))}
         </Box>
-        <Box mt={[8, 8, 8, 0]}>
+        <Box mt={[8, 8, 8, 0]} ml={15} >
           <Link
             to={link}
             py={3}
