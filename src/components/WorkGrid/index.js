7d0f3b05e2bcb4012c9967c8ed6aceca45c6d495
Cesar Benavides <cesarbenavides@icloud.com>
2021-06-14 12:09:24 -0500

    base work page

---
+++ src/components/WorkGrid/index.js
@@ -0,0 +1,55 @@
+import { Box, Grid, GridItem, Text, Container } from "@chakra-ui/layout"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import React from "react"
+import Link from "../Link/Link"
+
+const WorkGrid = ({ items }) => {
+  return (
+    <Container py={4}>
+      <Grid
+        templateColumns={[`repeat(2, 1fr)`, `repeat(2, 1fr)`, `repeat(3, 1fr)`]}
+        gap={4}
+      >
+        {items.map(item => (
+          <GridItem key={item.title} _hover={{ color: "secondary" }}>
+            <Link
+              to={item.link.url}
+              display="grid"
+              position="relative"
+              color={`white`}
+              role="group"
+              _hover={{ color: "secondary" }}
+              aria-label={item.title}
+            >
+              <Box
+                display="flex"
+                style={{ gridArea: `1/1` }}
+                h="100%"
+                zIndex={2}
+                p={6}
+              >
+                <Text
+                  alignSelf="flex-end"
+                  fontSize={[`sm`, `md`, `xl`]}
+                  fontWeight="bold"
+                  transition={`all .3s ease-in-out`}
+                  textTransform="uppercase"
+                  _groupHover={{ marginBottom: 5 }}
+                >
+                  {item.title}
+                </Text>
+              </Box>
+              <GatsbyImage
+                image={getImage(item.image.localFile.childImageSharp)}
+                alt={item.title}
+                style={{ gridArea: `1/1` }}
+              />
+            </Link>
+          </GridItem>
+        ))}
+      </Grid>
+    </Container>
+  )
+}
+
+export default WorkGrid
---
+++ src/pages/our-work.js
@@ -0,0 +1,105 @@
+import * as React from "react"
+import Layout from "../components/Layout/Layout"
+import { graphql } from "gatsby"
+import Seo from "gatsby-plugin-wpgraphql-seo"
+import GenericHero from "../components/GenericHero"
+import { ctaItems, ctaLink, ctaText } from "../constants/cta"
+import PrimaryCTA from "../components/PrimaryCTA"
+import WorkGrid from "../components/WorkGrid"
+
+const OurWorkPage = ({ data }) => {
+  // Hero Fields:
+  const heroTitle = data.wpPage.ourWorkFields.workHero.title
+  const heroImage =
+    data.wpPage.ourWorkFields.workHero.backgroundImage.localFile.childImageSharp
+  // Work Grid
+  const workItems = data.wpPage.ourWorkFields.workGrid.gridItems
+  return (
+    <Layout>
+      <Seo post={data.wpPage} />
+      <GenericHero title={heroTitle} image={heroImage} />
+      <PrimaryCTA items={ctaItems} link={ctaLink} ctaText={ctaText} />
+      <WorkGrid items={workItems} />
+    </Layout>
+  )
+}
+
+export default OurWorkPage
+
+export const workPageQuery = graphql`
+  query GET_WORK_PAGE {
+    wpPage(title: { eq: "Work" }) {
+      seo {
+        title
+        metaDesc
+        focuskw
+        metaKeywords
+        metaRobotsNoindex
+        metaRobotsNofollow
+        opengraphTitle
+        opengraphDescription
+        opengraphImage {
+          altText
+          sourceUrl
+          srcSet
+        }
+        twitterTitle
+        twitterDescription
+        twitterImage {
+          altText
+          sourceUrl
+          srcSet
+        }
+        canonical
+        cornerstone
+        schema {
+          articleType
+          pageType
+          raw
+        }
+      }
+      title
+      uri
+      nodeType
+      ourWorkFields {
+        workGrid {
+          gridItems {
+            title
+            link {
+              target
+              title
+              url
+            }
+            image {
+              localFile {
+                childImageSharp {
+                  gatsbyImageData(
+                    quality: 90
+                    placeholder: BLURRED
+                    formats: [AVIF, WEBP]
+                    layout: CONSTRAINED
+                  )
+                }
+              }
+            }
+          }
+        }
+        workHero {
+          title
+          backgroundImage {
+            localFile {
+              childImageSharp {
+                gatsbyImageData(
+                  layout: CONSTRAINED
+                  quality: 90
+                  placeholder: BLURRED
+                  formats: [AVIF, WEBP]
+                )
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
