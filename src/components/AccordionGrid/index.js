aaaee7104143ca0b9a2496e154d95965ae9f2626
alexandrafossum <fossum.alexandra@gmail.com>
2021-06-16 11:01:35 -0500

    Media Buying images in. Responsive design good. Need to standardize image heights across devices. Pushing for office development

--- src/components/AccordionGrid/index.js
+++ src/components/AccordionGrid/index.js
@@ -1,31 +1,34 @@
 import * as React from "react"
-import { Container, Accordion, AccordionButton, AccordionItem, AccordionPanel, Box, Text, Grid, VStack } from "@chakra-ui/react"
+import { Container, Accordion, AccordionButton, AccordionItem, AccordionPanel, AccordionIcon, Box, Text, Grid, VStack } from "@chakra-ui/react"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
 const AccordionGrid = ({ grid }) => {
     return(
-        <Container className="accordionGridWrapper" my={2}>
+        <Container className="accordionGridWrapper" my={10}>
             <Grid templateColumns={[`repeat(1, 1fr)`, `repeat(2, 1fr)`, `repeat(3, 1fr)`, `repeat(4, 1fr)`]} gap={6}>
             {grid.map(item => (
-            <Accordion defaultIndex={[0]} allowMultiple>
-                <AccordionItem>
+            <Accordion allowMultiple>
+                <AccordionItem style={{ borderColor: `transparent` }}>
                     <AccordionButton>
                         <VStack>
                         <Box>
                         <GatsbyImage
+                        className="mediaBuyingImage"
                         image={getImage(item.mbImage.localFile.childImageSharp)}
                         alt={item.mbTitle}
-                        style={{ gridArea: "1/1", maxWidth: `100%` }}
+                        style={{ gridArea: "1/1" }}
+                        maxHeight={[`150px`, `200px`, `225px`, `300px`]}
+                        width="auto"
                         />
                         </Box>
                         <Box>
                         <Text mb={5} style={{ textTransform: `uppercase`, fontFamily: `Montserrat`, color: `#black`, fontSize: `25px`, lineHeight: `30px` }}>{item.mbTitle}</Text>
                         </Box>
+                        <AccordionIcon />
                         </VStack>
                     </AccordionButton>
-                    <AccordionPanel style={{ borderColor: `transparent` }} dangerouslySetInnerHTML={{ __html: item.mbContent }} />
+                    <AccordionPanel style={{ borderColor: `transparent` }} dangerouslySetInnerHTML={{ __html: item.mbContent }} pb={5} />
                 </AccordionItem>
-                {console.log(item)}
             </Accordion>
             ))}
             </Grid>
