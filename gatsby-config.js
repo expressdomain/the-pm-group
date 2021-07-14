1603d2237badc37542da2c0fafc6865e0bd6cc2e
Cesar Benavides <cesarbenavides@icloud.com>
2021-07-14 10:32:00 -0500

    tweaks

--- gatsby-config.js
+++ gatsby-config.js
@@ -134,6 +134,7 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-remove-fingerprints`,
     // `gatsby-plugin-gatsby-cloud`,
     `gatsby-plugin-preact`,
     `gatsby-plugin-sass`,
