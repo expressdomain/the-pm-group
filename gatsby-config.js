37bc2a6f2455c309df473d705d9bcbe7f4de93f5
Cesar Benavides <cesarbenavides@icloud.com>
2021-07-19 15:26:30 -0500

    adjusting sharp issues

--- gatsby-config.js
+++ gatsby-config.js
@@ -120,7 +120,21 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        // Defaults used for gatsbyImageData and StaticImage
+        // defaults: {},
+        // Set to false to allow builds to continue on image errors
+        failOnError: false,
+        // deprecated options and their defaults:
+        // base64Width: 20,
+        // forceBase64Format: ``, // valid formats: png,jpg,webp
+        // useMozJpeg: process.env.GATSBY_JPEG_ENCODER === `MOZJPEG`,
+        // stripMetadata: true,
+        // defaultQuality: 50,
+      },
+    },
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
