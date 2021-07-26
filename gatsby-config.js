e97b294c17218cc6ad03c2ea7717a060c9affb0f
Cesar Benavides <cesarbenavides@icloud.com>
2021-07-26 17:03:03 -0500

    tweaks and stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -161,7 +161,7 @@ module.exports = {
     // `gatsby-plugin-gatsby-cloud`,
     // `gatsby-plugin-preact`,
     `gatsby-plugin-sass`,
-    // `gatsby-plugin-netlify`,
+    `gatsby-plugin-netlify`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
