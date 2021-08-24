4618d3d92fdcc8acfb9f1f2b98fa86dab60eae41
Cesar Benavides <cesarbenavides@icloud.com>
2021-08-24 12:03:28 -0500

    encoding

--- src/templates/category.js
+++ src/templates/category.js
@@ -45,7 +45,6 @@ const Category = data => {
             //   {description}
             // </Text>
           )}
-          {console.log(description)}
         </Box>
         <Box>
           <Grid
