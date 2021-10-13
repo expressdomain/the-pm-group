9c9fddc031acc4a284559658b841444960851479
Ray Anthony <16364682+Ray-Anthony@users.noreply.github.com>
2021-10-13 13:22:15 -0500

    Add breadcrumb fix for Newswire page

--- src/templates/news.js
+++ src/templates/news.js
@@ -23,6 +23,31 @@ const NewsWire = ({ data, pageContext }) => {
   const prevPage = currentPage - 1 === 1 ? "" : (currentPage - 1).toString()
   const nextPage = (currentPage + 1).toString()
 
+  // Replace all instances of '"/"' in seo.schema.raw with '"https://thepmgrp.com/"'
+  const schemaRaw = data.wpPage.seo.schema.raw.replace(/"\/"/g, '"https://thepmgrp.com/"')
+  // Initalize schema object
+  const schemaObj = JSON.parse(schemaRaw)
+
+  // Modify breadcrumb list
+  const breadcrumbList = schemaObj["@graph"][3]
+  // breadcrumbList["@context"] = "https://schema.org"
+  delete breadcrumbList["@id"]
+  // Home
+  breadcrumbList["itemListElement"][0].item = {
+  "@id": `${breadcrumbList["itemListElement"][0].item}`,
+  name: "Home",
+  }
+  delete breadcrumbList["itemListElement"][0].name
+  // News
+  breadcrumbList["itemListElement"][1].item = {
+  "@id": `https://thepmgrp.com/${data.wpPage.slug}/`,
+  name: data.wpPage.title,
+  }
+  delete breadcrumbList["itemListElement"][1].name
+
+  data.wpPage.seo.schema.raw = JSON.stringify(schemaObj)
+  console.log(schemaObj);
+
   return (
     <Layout>
       <Seo post={data?.wpPage} />
@@ -197,6 +222,7 @@ export const newsQuery = graphql`
           raw
         }
       }
+      slug
       newsPageFields {
         newsPageHero {
           heroImage {
